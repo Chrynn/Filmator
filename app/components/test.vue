d7f8201df789025d5d85d963774cf1526179b14e
Filip Machala <filip@dhcp-192-168-30-116.in.webwings.cz>
2023-06-01 09:41:31 +0200

    webpack scripts added, manifest js setting added

--- app/components/test.vue
+++ app/components/test.vue
@@ -3,11 +3,3 @@
         <h1>hello</h1>
     </div>
 </template>
-
-<script>
-
-</script>
-
-<style>
-
-</style>
--- webpack.config.js
+++ webpack.config.js
@@ -5,14 +5,26 @@ if (!Encore.isRuntimeEnvironmentConfigured()) {
 }
 
 Encore
+    // where webpack files are generated
     .setOutputPath('./www/dist')
+    // where webpack generated files are placed to use by webpack
     .setPublicPath('/dist')
+    // where Vue config file is
     .addEntry('main', './www/js/main.js')
     .enableSingleRuntimeChunk()
     .cleanupOutputBeforeBuild()
     .enableBuildNotifications()
     .enableSourceMaps(!Encore.isProduction())
     .enableVersioning(Encore.isProduction())
-    .enableVueLoader(() => {}, { runtimeCompilerBuild: true });
+    .enableVueLoader(() => {}, { runtimeCompilerBuild: true })
+    // setting manifest files 1 folder back
+    .configureManifestPlugin(options => {
+        options.publicPath = '';
+    })
+    .configureDevServerOptions(options => {
+        options.port = 90;
+        options.allowedHosts = 'all';
+    })
+;
 
 module.exports = Encore.getWebpackConfig();
